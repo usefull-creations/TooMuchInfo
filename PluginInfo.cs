8feac8480f06f814fd42a1d3e39e0694f1119c94
poopooVR <blackusmokemefr@gmail.com>
2026-08-31 03:20:28 +0100

    Update plugin GUID and version number

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,8 +5,8 @@
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.goldentrophy.gorillatag.toomuchinfo";
+        public const string GUID = "com.poopoovr.gorillatag.toomuchinfo";
         public const string Name = "TooMuchInfo";
-        public const string Version = "1.2.3";
+        public const string Version = "1.2.4";
     }
 }
